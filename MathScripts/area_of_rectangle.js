6db92db95dc1122232f4341f83527f5320ac181b
Alejandro Regus <Aregus@appdev.yearup.org>
2023-04-24 23:00:30 -0400

    area-of-rectangle-and-net-worth-js-complete

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+//sample inputs
+var length = 8;
+var width = 4;
+
+//calculation
+var rectangleArea = length * width;
+
+//output
+console.log("The area of the rectangle is " + rectangleArea)
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,10 @@
+//sample inputs
+var PKMcards = 300.05;
+var Electronics = 1000;
+var Billnearme = 64;
+
+//calculations
+var Networth = PKMcards + Electronics - 64;
+
+//output
+console.log("Your net worth is " + Networth);
