368bbf0ca1e459049cbadd9f188cc2c7ae07d486
Alejandro Regus <Aregus@appdev.yearup.org>
2023-04-25 09:45:51 -0400

    area-of-circle-js-updated

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -3,6 +3,6 @@ var radius = 4;
 
 //calculations
 var diameter = radius ** 2;
-var area = 3.14 * diameter;
+var area = Math.PI * diameter;
 //output
 console.log("The area of a circle with radius " + radius +" is " + area)
