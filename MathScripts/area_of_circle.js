baaac795f1ef71977bf8b4f4c2843a9fc6930554
Alejandro Regus <Aregus@appdev.yearup.org>
2023-04-25 09:16:38 -0400

    area-of-circle-js-complete

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -1,5 +1,8 @@
 //sample input
+var radius = 4;
 
 //calculations
-
+var diameter = radius ** 2;
+var area = 3.14 * diameter;
 //output
+console.log("The area of a circle with radius " + radius +" is " + area)
